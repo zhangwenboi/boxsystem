963195e3ad55e2f863143e25092715b769b3b395
张文博 <1953954864@qq.com>
2024-10-09 08:56:15 +0800

    add

--- douyin.js
+++ douyin.js
@@ -2,83 +2,129 @@
 
 // /** @format */
 
-// const http = require('http');
-// const https = require('https');
+const http = require('http');
+const https = require('https');
 
-// // 统计总共下载的字节数(mb)
-// let totalDownloadedBytes = 0;
+// 统计总共下载的字节数(mb)
+let totalDownloadedBytes = 0;
 
-// const fileUrls = ['http://v5-dy-o-abtest.zjcdn.com/f07bfa520c1212985ffee08108a2b9fe/66f96855/video/tos/cn/tos-cn-ve-15/oYbApfDtFH0mZrNH79JkABsAZo5QJCAfgmnR4E/'];
-// let progresscurrentchunks = 0;
-// let alldata = 0;
-// const downloadFileContent = (fileUrl) => {
-//   try {
-//     return new Promise((resolve, reject) => {
-//       const protocol = fileUrl.startsWith('https') ? https : http;
+let progresscurrentchunks = 0;
+let alldata = 0;
 
-//       progresscurrentchunks = 0;
-//       let timer = setTimeout(() => {
-//         resolve('超时56' + fileUrl);
-//       }, 20000);
-//       // 发送 HTTP 请求
-//       const req = protocol
-//         .get(fileUrl, (response) => {
-//           alldata = Number((response.headers['content-length'] / 1024 / 1024)?.toFixed(2));
-//           console.log('本次将要下载' + alldata + 'MB');
-//           response.on('data', (chunk) => {
-//             progresscurrentchunks += Number(chunk.length);
-//           });
-//           response.on('end', () => {
-//             totalDownloadedBytes += alldata;
-//             resolve('成功');
-//             clearTimeout(timer);
-//           });
-//           if (alldata > 0) {
-//             clearTimeout(timer);
-//             timer = setTimeout(() => {
-//               resolve('超时56' + fileUrl);
-//             }, alldata * 300);
-//           }
-//         })
-//         .on('error', (err) => {
-//           resolve('失败70' + err);
-//         })
-//         .on('timeout', (err) => {
-//           resolve('超时53' + err);
-//         });
+const getFileData = () => {
+  try {
+    const res = fs.readFileSync('runtimer.json', 'utf8');
+    return JSON.parse(res);
+  } catch (error) {
+    return { start: '0:0', end: '0:0' };
+  }
+};
+const canRun = () => {
+  const startEnd = getFileData();
+  if (startEnd?.start === startEnd?.end) {
+    return true;
+  }
 
-//       req.setTimeout(5000, (err) => {
-//         resolve('超时80', err);
-//       });
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+  // 获取当前时间的小时和分钟
+  const now = new Date();
+  const currentHour = now.getHours();
+  const currentMinute = now.getMinutes();
+  // 定义时间范围
+  const [startHour, startMinute] = startEnd?.start?.split(':')?.map(Number);
+  const [endHour, endMinute] = startEnd?.end?.split(':')?.map(Number);
 
-// const downloadAllFilesContent = async (currentIndex = 0) => {
-//   while (true) {
-//     try {
-//       const fileUrl = fileUrls[currentIndex];
-//       const res = await downloadFileContent(fileUrl);
-//       console.log(`${res}一共下载了${currentIndex} | ${totalDownloadedBytes}MB \n`);
-//       currentIndex = (currentIndex + 1) % fileUrls.length;
-//     } catch (error) {
-//       const fileUrl = fileUrls[currentIndex];
-//       await downloadFileContent(fileUrl);
-//       console.log(`下载${fileUrl}失败${error}`);
-//       currentIndex = (currentIndex + 1) % fileUrls.length;
-//     }
-//   }
-// };
-// const ttttttt = setInterval(() => {
-//   console.log(`${(progresscurrentchunks / 1024 / 1024).toFixed(2)} / ${alldata}`);
-// }, 1000);
+  // 判断当前时间是否在时间范围内
+  if ((currentHour > startHour || (currentHour === startHour && currentMinute >= startMinute)) && (currentHour < endHour || (currentHour === endHour && currentMinute <= endMinute))) {
+    return true;
+  } else {
+    return false;
+  }
+};
+const waitTimer = async (time) => {
+  return await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
+const requestData = async (url, callback) => {
+  const protocol = url.startsWith('https') ? https : http;
+  let timer = setTimeout(() => {
+    resolve('超时56' + url);
+  }, 20000);
+  // 发送 HTTP 请求
+  const req = protocol
+    .get(url, callback)
+    .on('error', (err) => {
+      clearTimeout(timer);
+      resolve('失败70' + err);
+    })
+    .on('timeout', (err) => {
+      clearTimeout(timer);
+      resolve('超时53' + err);
+    });
 
-// downloadAllFilesContent(0);
+  req.setTimeout(5000, (err) => {
+    resolve('超时80', err);
+  });
+};
+const downloadFileContent = (fileUrl) => {
+  try {
+    return new Promise((resolve, reject) => {
+      requestData(fileUrl, (response) => {
+        alldata = Number((response.headers['content-length'] / 1024 / 1024)?.toFixed(2));
+        console.log('本次将要下载' + alldata + 'MB');
+        response.on('data', (chunk) => {
+          progresscurrentchunks += Number(chunk.length);
+        });
+        response.on('end', () => {
+          totalDownloadedBytes += alldata;
+          resolve('成功');
+          clearTimeout(timer);
+        });
+        if (alldata > 0) {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+            resolve('超时56' + fileUrl);
+          }, alldata * 300);
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// process.on('uncaughtException', (err) => {
-//   console.log('An uncaught exception occurred:', err);
-//   clearInterval(ttttttt);
-//   downloadAllFilesContent(0);
-// });
+const downloadAllFilesContent = async (currentIndex = 0) => {
+  while (true) {
+    const fileUrls = [];
+    requestData('https://gitee.com/wen_wen_okok/boxurl/raw/master/boxurl/data.txt', (response) => {
+      console.log(response);
+    });
+    // try {
+    //   if (!canRun()) {
+    //     console.log('不在运行时间！');
+    //     await waitTimer(10000);
+    //     continue;
+    //   } else {
+    //     progresscurrentchunks = 0;
+    //     const fileUrl = fileUrls[currentIndex];
+    //     const res = await downloadFileContent(fileUrl);
+    //     console.log(`${res}一共下载了${currentIndex} | ${totalDownloadedBytes}MB \n`);
+    //     currentIndex = (currentIndex + 1) % fileUrls.length;
+    //   }
+    // } catch (error) {
+    //   const fileUrl = fileUrls[currentIndex];
+    //   await downloadFileContent(fileUrl);
+    //   console.log(`下载${fileUrl}失败${error}`);
+    //   currentIndex = (currentIndex + 1) % fileUrls.length;
+    // }
+  }
+};
+
+downloadAllFilesContent(0);
+
+process.on('uncaughtException', (err) => {
+  console.log('An uncaught exception occurred:', err);
+  //   downloadAllFilesContent(0);
+});
